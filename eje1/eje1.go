d1e5fa241841d98eb72cb379f189e8aea9eed905
Atilio <atilioboher@gmail.com>
2022-05-23 23:57:42 -0300

    se arregló el eje1

--- eje1/eje1.go
+++ eje1/eje1.go
@@ -11,16 +11,18 @@ func openFileRecoverFunc() {
 	}
 }
 
-func openFile(fileName string) {
-	_, err := os.Open(fileName)
+func openFile(fileName string) *os.File {
+	myFile, err := os.Open(fileName)
 	defer fmt.Println("Ejecución finalizada")
 	defer openFileRecoverFunc()
 	if err != nil {
 		panic("Error: el archivo indicado no fue encontrado o está dañado")
 	}
+	return myFile
 }
 
 func main() {
-	openFile("customers.txt")
+	myFile := openFile("eje1/customers.txt") // la forma correcta de escribir la dirección es "eje1/customers.txt"
+	defer myFile.Close()
 	fmt.Println("la ejecución del programa sigue")
 }
