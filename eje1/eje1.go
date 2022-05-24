733160b15bc50d70de9c84ac54f3bcc77b4ae978
Atilio <atilioboher@gmail.com>
2022-05-23 23:11:41 -0300

    se terminó el eje1

--- eje1/eje1.go
+++ eje1/eje1.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func openFileRecoverFunc() {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+	}
+}
+
+func openFile(fileName string) {
+	_, err := os.Open(fileName)
+	defer fmt.Println("Ejecución finalizada")
+	defer openFileRecoverFunc()
+	if err != nil {
+		panic("el archivo indicado no fue encontrado o está dañado")
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	openFile("customers.txt")
+	fmt.Println("la ejecución del programa sigue")
 }
