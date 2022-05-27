5dcd24f3408ccc616852b8b4679ee51d853cb407
Atilio <atilioboher@gmail.com>
2022-05-27 12:55:35 -0300

    se agregó la función que checkea si un usuario existe y que el Legajo autogenerado sea distinto a los ya existentes

--- eje2/eje2.go
+++ eje2/eje2.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -23,12 +24,26 @@ type cliente struct {
 }
 
 func generateIdNumber() (int, error) {
-	randNum := rand.Intn(max-min) + min
-	// randNum = 3 // uncomment this line to test the panic
-	if randNum < min || randNum > max {
-		return 0, fmt.Errorf("el número de legajo generado es %d, el cual se encuentra fuera del rango permitido (%d-%d)", randNum, min, max)
+	band := true
+	var randNum int
+	for band {
+		randNum = rand.Intn(max-min) + min
+		// randNum = 3 // uncomment this line to test the panic
+		if randNum < min || randNum > max {
+			return 0, fmt.Errorf("el número de legajo generado es %d, el cual se encuentra fuera del rango permitido (%d-%d)", randNum, min, max)
+		}
+		band = checkIfIdNumberExist(randNum) // repeats until the genereted number is different from the already existing ones
 	}
 	return randNum, nil
+}
+
+func checkIfIdNumberExist(id int) bool {
+	aux, err := os.ReadFile("eje2/customers.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	data := string(aux)
+	return strings.Contains(data, "Legajo: "+strconv.Itoa(id))
 }
 
 func openFileRecoverFunc() {
@@ -47,16 +62,29 @@ func openFile(fileName string) *os.File {
 	return myFile
 }
 
+func checkIfClientExist(c cliente) bool {
+	aux, err := os.ReadFile("eje2/customers.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	data := string(aux)
+	// fmt.Println(data)
+	condition1 := strings.Contains(data, "Nombre y Apellido: "+c.nombYApell)
+	condition2 := strings.Contains(data, "DNI: "+strconv.Itoa(c.dni))
+	return condition1 && condition2 // name and DNI has to be the same, the rest could be checked, but I consider is not necessary
+}
+
 func addClient(c cliente) {
-	f := openFile("tomers.txt") // the correct way to type the path is like this "eje2/customers.txt"
+	f := openFile("eje2/customers.txt") // the correct way to type the path is like this "eje2/customers.txt"
 	defer f.Close()
 
 	if f != nil {
-		if _, err := f.Write([]byte(strconv.Itoa(c.legajo) + "\n" +
-			c.nombYApell + "\n" +
-			strconv.Itoa(c.dni) + "\n" +
-			strconv.Itoa(c.telef) + "\n" +
-			c.domic + "\n\n")); err != nil {
+
+		if _, err := f.Write([]byte("Legajo: " + strconv.Itoa(c.legajo) + "\n" +
+			"Nombre y Apellido: " + c.nombYApell + "\n" +
+			"DNI: " + strconv.Itoa(c.dni) + "\n" +
+			"Telefono: " + strconv.Itoa(c.telef) + "\n" +
+			"Domicilio: " + c.domic + "\n\n")); err != nil {
 			fmt.Println(err)
 		}
 	}
@@ -66,18 +94,21 @@ func addClient(c cliente) {
 func main() {
 	rand.Seed(time.Now().UnixNano()) // this is here to have a different auto generated ID each time we run the program
 
-	id, err := generateIdNumber()
+	var (
+		cliente1 cliente
+		err      error
+	)
+
+	cliente1.legajo, err = generateIdNumber()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("se generó el siguiente legajo: %d \n", id)
-
-	var cliente1 cliente
-	cliente1.legajo, _ = generateIdNumber()
-	cliente1.nombYApell = "un nombre y apellido"
+	fmt.Printf("se generó el siguiente legajo: %d \n", cliente1.legajo)
+	cliente1.nombYApell = "un nombre y apellido1"
 	cliente1.dni = 40358487
 	cliente1.telef = 15783828
 	cliente1.domic = "calle tanto"
-	addClient(cliente1)
-	fmt.Println("el programa sigue")
+	// addClient(cliente1)
+	fmt.Println("----------------------------")
+	fmt.Println(checkIfClientExist(cliente1))
 }
